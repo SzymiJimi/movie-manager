8dc4e2050f02bacfbf99e925b701995a34654534
SzymiJimi <rekas1@tlen.pl>
2018-04-26 15:52:14 +0200

    Initial commit

---
+++ src/main/java/com/szymijimi/moviemanager/MovieManagerApplication.java
@@ -0,0 +1,12 @@
+package com.szymijimi.moviemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieManagerApplication.class, args);
+	}
+}
